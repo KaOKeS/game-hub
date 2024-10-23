f060c5e8d09368d1f9021985fd5dd528c4d3e4af
Dawid P. <pater.dawid92@gmail.com>
2024-10-23 20:21:49 +0200

    Filter games by platfrom.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,6 @@
 import useData from "./useData";
 import { Genre } from "./useGenres";
-
-export interface Platform{
-    id:number;
-    name:string;
-    slug:string;
-}
+import { Platform } from "./usePlatforms";
 
 export interface Game {
     id: number;
@@ -16,6 +11,12 @@ export interface Game {
   }
   
 
-const useGames = (selectedGenre : Genre | null) => useData<Game>('/games',{params: {genres: selectedGenre?.id}},[selectedGenre?.id])
+const useGames = (selectedGenre : Genre | null, selectedPlatform: Platform | null) => 
+  useData<Game>('/games', {
+    params: {
+      genres: selectedGenre?.id, 
+      platforms: selectedPlatform?.id
+    }},
+    [selectedGenre?.id,selectedPlatform?.id])
 
 export default useGames;
