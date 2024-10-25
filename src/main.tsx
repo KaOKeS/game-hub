7e92a52f704ed72ca5b51443653f366c18998657
Dawid P. <pater.dawid92@gmail.com>
2024-10-25 23:19:27 +0200

    ReactQuery installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+        <ReactQueryDevtools />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
